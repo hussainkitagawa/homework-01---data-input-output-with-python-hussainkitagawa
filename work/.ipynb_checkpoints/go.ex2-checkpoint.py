e1fad9459f389057a6055d23aa75edf27acb678a
Hussain Kitagawa <dfiyz866@gmail.com>
2023-06-20 14:35:27 +0000

    Hussain Kitagawa: Task Finished!

---
+++ work/.ipynb_checkpoints/go.ex2-checkpoint.py
@@ -0,0 +1,10 @@
+#import Numpy
+import os
+import numpy as np
+
+# Import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm matplotlib inline
+
+#Now, import the FITS module inside Astropy
+from astropy.io import fits
